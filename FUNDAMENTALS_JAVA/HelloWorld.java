ec6686f22cb25860b699d514f5e9bee622487568
ifelipesilva <ifelipesilvapro@gmail.com>
2024-02-28 17:38:08 -0300

    first commit

---
+++ FUNDAMENTALS_JAVA/HelloWorld.java
@@ -0,0 +1,7 @@
+package FUNDAMENTALS_JAVA;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+<h2 align="center">⚜️ Java Learning Testing
+<i><h4 align="center">Self-study using Java programming learning resources.</i> 
+ 
+##   
+
