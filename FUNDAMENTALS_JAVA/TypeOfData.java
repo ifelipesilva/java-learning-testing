ec790e8d76b61abb9ff09fe0ca9cdb09cf063fea
ifelipesilva <ifelipesilvapro@gmail.com>
2024-02-28 19:34:37 -0300

    type of data

---
+++ FUNDAMENTALS_JAVA/TypeOfData.java
@@ -0,0 +1,24 @@
+package FUNDAMENTALS_JAVA;
+
+public class TypeOfData {
+  //Values (int, double, float, long)
+  //Text (String)
+  //Bool (boolean)
+
+  public static void main(String[] args) {
+    
+    int valueInt = 50;
+    double valueDouble = 10.5;
+    float valueFloat = 10.5f;
+    long valueLong = 10L;
+    String text = "Hello, World!";
+    boolean valueBoolean = true;
+
+    System.out.println(valueInt);
+    System.out.println(valueDouble);
+    System.out.println(valueFloat);
+    System.out.println(valueLong);
+    System.out.println(text);
+    System.out.println(valueBoolean);
+  }
+}
